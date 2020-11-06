4308be5d24c1a0c47be1b5e53a707a4dabe6b1c0
klory <hfdtsinghua@gmail.com>
2020-11-06 11:48:23 -0500

    remove duplicate metrics

--- generative models/metrics/__init__.py
+++
@@ -1,4 +0,0 @@
-from .frechet_inception_distance import FIDModel, compute_fid
-from .inception_score import INCEPTION_V3, compute_is
-from .msssim import compute_msssim
-from .ndcg import compute_nDCG_by_sim
